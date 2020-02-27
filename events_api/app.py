ae717a433bc85441a43fca01fbb9e65f3fecfb03
alpden550 <alpden550@gmail.com>
2020-02-27 11:25:25 +0300

    Added settings.

--- events_api/app.py
+++ events_api/app.py
@@ -1,8 +1,11 @@
 import os
 
+import click
 from flask import Flask
 
 from events_api.blueprints.api import api_bp
+from events_api.extensions import db
+from events_api.settings import config
 
 
 def create_app(config_name=None):
@@ -10,7 +13,23 @@ def create_app(config_name=None):
         config_name = os.getenv('FLASK_CONFIG', 'production')
 
     app = Flask(__name__)
+    app.config.from_object(config[config_name])
 
     app.register_blueprint(api_bp, url_prefix='/api')
+    register_extensions(app)
+    register_commands(app)
 
     return app
+
+
+def register_extensions(app):
+    db.init_app(app)
+
+
+def register_commands(app):
+    @app.cli.command()
+    def init():
+        """Create empty database."""
+        db.drop_all()
+        db.create_all()
+        click.echo('Initialized empty database.')
---
+++ events_api/settings.py
@@ -0,0 +1,32 @@
+import os
+from pathlib import Path
+
+BASE_DIR = Path().resolve()
+
+
+class BaseConfig(object):
+    DEBUG = os.getenv('DEBUG') in {'1', 'yes', 'true', 'True'}
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///{db}'.format(db=BASE_DIR.joinpath('events.sqlite.db'))
+
+
+class DevelopmentConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///{db}'.format(db=BASE_DIR.joinpath('dev.sqlite.db'))
+
+
+class TestingConfig(BaseConfig):
+    TESTING = True
+    SECRET_KEY = 'extrasecretstring'  # noqa:S105
+    WTF_CSRF_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # in-memory database
+
+
+config = {
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'development': DevelopmentConfig,
+}
