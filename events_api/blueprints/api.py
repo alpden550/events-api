a2cc922f6b905ef4d23e53ad27fe3307d196db10
alpden550 <alpden550@gmail.com>
2020-02-28 21:39:42 +0300

    Added post enrollment.

--- events_api/blueprints/api.py
+++ events_api/blueprints/api.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, jsonify, request
 from sqlalchemy.orm import joinedload
 
-from events_api.models import Event, Location
+from events_api.extensions import csrf
+from events_api.models import Enrollment, Event, Location, Participant
 from events_api.schema_models import EventSchema, LocationSchema
+from events_api.extensions import db
 
 api_bp = Blueprint('api', __name__)
+csrf.exempt(api_bp)
 
 
 @api_bp.route('/')
@@ -36,9 +39,27 @@ def get_events():
     return jsonify(events_json), 200
 
 
-@api_bp.route('/enrollments/<int:eventid>', methods=['GET'])
+@api_bp.route('/enrollments/<int:eventid>', methods=['POST'])
 def get_enrollment(eventid):
-    return jsonify({'status': 'success'})
+    event = Event.query.get(eventid)
+    if len(event.enrollments) > event.seats:
+        return jsonify({'status': 'error, not seats'}), 500
+
+    participant = Participant.query.filter_by(email=request.json.get('email')).first()
+    if participant is None:
+        return jsonify({'status': 'error, email does not exist'}), 400
+    if participant.id in (participant.id for participant in event.participants):
+        return jsonify({'status': 'error, user already enrrolled'}), 500
+
+    enrollment = Enrollment.create(event=event, participant=participant)
+    event.participants.append(participant)
+    db.session.add(event)
+    db.session.commit()
+    return (
+        jsonify({'status': 'success'}),
+        201,
+        {'Location': f'/enrollments/{enrollment.id}'},
+    )
 
 
 @api_bp.route('/enrollments/<int:eventid>', methods=['DELETE'])
